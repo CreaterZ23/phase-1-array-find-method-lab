5b67e10b008689fdb5b582fe0e74e2cad72ffd6f
Zeon <zeonstewart23@gmail.com>
2021-08-12 11:40:21 -0400

    commit tree 93de7b95bc4a3e5c5e69c74602c494f1b743aa32

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+// const record = [
+//     { year: 2018, result: "L" }, { year: 2017, result: "W" }, { year: 2016, result: "N/A" }
+// ];
 
+// function superbowlWin(record) {
+//     if (record.result === "W") {
+//         return record.year;
+//     }
+// }
+
+
+
+// record.find(superbowlWin);
+
+
+function superbowlWin(recordArr) {
+    const lastWinObj = recordArr.find(season => season.result === "W");
+    console.log(lastWinObj);
+    return lastWinObj ? lastWinObj.year : undefined;
+
+
+}
